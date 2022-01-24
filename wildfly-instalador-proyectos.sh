6ce4988e201a658d1c600d7635f7a605b6a4bc3e
juanitovcanto <diego.valenzuela.canto@gmail.com>
2022-01-23 21:34:05 -0300

    Modificacion de instalador sin properties

--- wildfly-instalador-proyectos.sh
+++ wildfly-instalador-proyectos.sh
@@ -14,22 +14,24 @@
 #-----------------------------------------------------------------------------
 # Decalaracion variables de tiempo real
 FECHA_HORA_LOG=$(date +%d-%m-%Y-%H_%M_%S)
-PROCESS_ID="jboss-instala-componentes"
-USER=jboss
-ROOT=instalador-jboss
-PROPERTIES=Properties 
+PROCESS_ID="wildfly-instala-componentes"
+DIRECTORIO_WILDFLY="wildfly-preview-22.0.0.Final"
 
 #-----------------------------------------------------------------------------
 # Decalaracion de directorios y archivos base
 #-----------------------------------------------------------------------------
 
-export XPP_HOME=/home/$USER/$ROOT
+export XPP_HOME=~/instaladorWildfly
 export XPP_KSH=$XPP_HOME/sh
 export XPP_LOG=$XPP_HOME/log
-export XPP_JBOSS_CLI=/opt/wildfly/bin/jboss-cli.sh
+
+#######
+# DIRECTORIO DE WILDFLY
 
 
-export XPP_JBOSS_PROPERTIES=/opt/jboss-eap-7.0/modules/bancapersona/main/properties
+
+export XPP_JBOSS_CLI=/opt/$DIRECTORIO_WILDFLY/bin/jboss-cli.sh
+
 
 # Archivo de LOG
 LOG_FILE="$XPP_LOG/${PROCESS_ID}_${FECHA_HORA_LOG}.log"
@@ -38,9 +40,9 @@ LOG_FILE="$XPP_LOG/${PROCESS_ID}_${FECHA_HORA_LOG}.log"
 WAR="$1"
 DIR_FECHA="$2"
 REPOSITORY="$3"
-REPO_CHANGES="$4 $5"
 
-export XPP_WARS=/home/jboss/COMPILADOS/BACKEND/$REPOSITORY #  UBICACION DE WARS EN JBOSS
+export XPP_COMPILADOS=~/COMPILADOS
+export XPP_WARS=$XPP_COMPILADOS/$REPOSITORY #  UBICACION DE WARS EN JBOSS
 
 #-----------------------------------------------------------------------------
 ################## VARIABLES GLOBALES
@@ -49,24 +51,25 @@ export XPP_WARS=/home/jboss/COMPILADOS/BACKEND/$REPOSITORY #  UBICACION DE WARS 
 JBOSS_CLI_CHECK_DEPLOYMENT=$($XPP_JBOSS_CLI --connect --commands=ls\ deployment | grep -w $WAR)
 JBOSS_CLI_UNDEPLOY=$($XPP_JBOSS_CLI --connect --commands=undeploy\ $WAR)
 JBOSS_CLI_DEPLOY=$($XPP_JBOSS_CLI --connect --commands=deploy\ $XPP_WARS/$DIR_FECHA/$WAR)
-JBOSS_CLI_STATUS="systemctl is-active --quiet jboss-eap.service"
+JBOSS_CLI_STATUS="systemctl --user is-active --quiet wildfly.service"
 
 JBOSS_CLI_DISABLE=$($XPP_JBOSS_CLI --connect --commands=undeploy\ --keep-content\ $WAR)
 JBOSS_CLI_ENABLE=$($XPP_JBOSS_CLI --connect --commands=deploy\ --name=$WAR)
 
-
-PROPERTIES_FILES=$(ls $XPP_WARS/$DIR_FECHA/$PROPERTIES | egrep '.properties$')
-
-#JBOSS_CLI_STATUS="nc -zw1 10.250.12.164 9990" ----> alternativa con comando ncat (nc)
 
 # Expresión regular para validar formato de archivos
 # Anadir otro formato dentro del parentesis escribiendo "|formato" EJ: (war|jar|ear|gz) 
 REGEX_FILE_FORMAT='\.(war|jar|ear)$'
 
 
-MATCH_TWO=$(echo $REPO_CHANGES | grep -P '^(?=.*Fuentes)(?=.*Properties)')
-MATCH_FUENTES=$(echo $REPO_CHANGES | grep -e "Fuentes" )
-MATCH_PROPERTIES=$(echo $REPO_CHANGES | grep -e "Properties")
+#-----------------------------------------------------------------------------
+################### Diferenciacion para casos con Properties
+
+#MATCH_TWO=$(echo $REPO_CHANGES | grep -P '^(?=.*Fuentes)(?=.*Properties)')
+#MATCH_FUENTES=$(echo $REPO_CHANGES | grep -e "Fuentes" )
+#MATCH_PROPERTIES=$(echo $REPO_CHANGES | grep -e "Properties")
+
+#-----------------------------------------------------------------------------
 
 #-----------------------------------------------------------------------------
 ################# FIN VARIABLES GLOBALES
@@ -81,6 +84,15 @@ WriteLOG()
 {
     FECHA_HORA_LOG=$(date +[%d/%m/%Y][%X])
     echo "EVENTO $FECHA_HORA_LOG: "$1 >> $LOG_FILE
+}
+CheckDirectory(){
+    if [[ -d $XPP_COMPILADOS ]];then
+        WriteLOG "(info): directorio COMPILADOS existe"
+    else
+        WriteLOG "(info): directorio COMPILADOS no existe"
+        WriteLOG "(info): creando directorio...."
+        mkdir ~/COMPILADOS
+    fi
 }
 
 CheckFile(){
@@ -103,7 +115,7 @@ CheckFileFormat(){
     fi
 
 }
-
+<<properties
 CheckPropertiesExists(){
     WriteLOG "(info): verificando propertie "
     for propertie in $PROPERTIES_FILES
@@ -130,6 +142,7 @@ CheckPropertiesInstalled(){
         fi
     done   
 }
+properties
 
 CheckJbossConnection(){
     if $JBOSS_CLI_STATUS;then
@@ -140,6 +153,7 @@ CheckJbossConnection(){
     fi 
 
 }
+<<com
 ReloadDeployment(){
     WriteLOG "(info): desactivando deployment "
     if  $JBOSS_CLI_DISABLE  ; then 
@@ -158,7 +172,7 @@ ReloadDeployment(){
     fi 
 
 }
-
+com
 
 UninstallWar(){
     if [[ $JBOSS_CLI_CHECK_DEPLOYMENT ]]; then
@@ -184,7 +198,8 @@ CheckWarInstalled(){
     if [[ $JBOSS_CLI_CHECK_DEPLOYMENT ]]; then
 
         WriteLOG "(info): El servicio $WAR se encuentra instalado"
-        ReloadDeployment
+        UninstallWar
+        InstallWar
     else
         WriteLOG "(info): El servicio $WAR no se encuentra instalado"
         InstallWar
@@ -204,58 +219,21 @@ InstallWar(){
     fi 
 }
 
-CheckRepoChanges(){
-    if [[ $MATCH_TWO ]]; then
-        WriteLOG "(info): Cambios en Fuentes y Properties"
-
-        CheckFile
-        CheckFileFormat
-        CheckPropertiesExists
-        CheckJbossConnection
-        UninstallWar
-        InstallWar
-
-    elif [[ $MATCH_FUENTES ]]; then
-        WriteLOG "(info): Cambios en Fuentes"
-
-        CheckFile
-        CheckFileFormat
-        CheckJbossConnection
-        CheckPropertiesInstalled
-        UninstallWar
-        InstallWar
-
-    elif [[ $MATCH_PROPERTIES ]]; then
-        WriteLOG "(info): Cambios en Properties"
-        CheckPropertiesExists
-        CheckJbossConnection
-        CheckWarInstalled
-        
-    else
-        echo "No hubo cambios en Properties ni en Fuentes"
-    fi
-}
 ## Flujo de ejecucion
 
-# Paso 1 : Verifica que cambios hubo en el repositorio 
-CheckRepoChanges
+# Paso 1 : Verifica si existe directorio COMPILADOS
+CheckDirectory 
 
 # Paso 2: Verifica que exista el war enviado desde jenkins
-#CheckFile
+CheckFile
 # Paso 3: Verifica formato de archivos permitidos (.war, .jar, .ear)
-#CheckFileFormat
-# Paso 4: Verifica y mueve properties 
-#CheckPropertiesExists
-# Paso 5: Verifica si Jboss esta conectado
-#CheckJbossConnection
+CheckFileFormat
 
-# Paso 6: Verifica si el war ya se encuentra instalado en Jboss. Si se encuentra instalado, se desintala
-#UninstallWar
-# Paso 7: Instala la nueva version del war en Jboss
-#InstallWar
+# Paso 4: Chequea si el war se encuentra instalado
+CheckWarInstalled
 
-#CheckWarInstalled
-# Chequea si el war se encuentra instalado
+
+
 
 #ReloadDeployment
 # desactiva y vuelve a activar el War en deployment
